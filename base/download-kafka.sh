e1242f0fbc6034ffa8e3ba414d69fd6791ec30d2
Nicolas Brière <n.briere@smartway.ai>
2025-09-05 14:22:07 +0200

    chore(deps): update Kafka version to 4.1.0

--- base/download-kafka.sh
+++ base/download-kafka.sh
@@ -5,5 +5,4 @@ curl -fSL -o /tmp/kafka.tgz $(curl --stderr /dev/null https://www.apache.org/dyn
 echo "$SHA512HASH /tmp/kafka.tgz" | sha512sum -c - &&\
 tar -xzf /tmp/kafka.tgz -C $KAFKA_HOME --strip-components 1 &&\
 rm -f /tmp/kafka.tgz &&\
-zip -d /kafka/libs/reload4j-1.2.25.jar org/apache/log4j/net/JMSAppender.class org/apache/log4j/net/SocketServer.class org/apache/log4j/net/JMSSink.class 'org/apache/log4j/jdbc/*' 'org/apache/log4j/chainsaw/*' &&\
 chmod -R g+w,o+w $KAFKA_HOME
